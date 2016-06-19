ea8a9b02b7f62c7ca59a9a7a0e178719503fb362
arvin sharma <arvinshma@gmail.com>
2016-06-19 13:28:09 -0400

    nested notepanel and pitchpanel into the gui view frame

--- src/cs3500/music/view/GuiViewFrame.java
+++ src/cs3500/music/view/GuiViewFrame.java
@@ -1,8 +1,12 @@
 package cs3500.music.view;
 
 import java.awt.*;
+import java.awt.List;
 import java.awt.event.MouseListener; // Possibly of interest for handling mouse events
+import java.util.*;
+
 import javax.swing.*;
+
 import cs3500.music.model.MusicModelObserver;
 import cs3500.music.model.Note;
 
@@ -17,6 +21,7 @@ public class GuiViewFrame extends javax.swing.JFrame
   private int maxBeats;
   private JPanel mainPanel;
   private JScrollBar sb;
+
   /**
    * Creates new GuiView
    */
@@ -43,10 +48,106 @@ public class GuiViewFrame extends javax.swing.JFrame
     sb.setValue(position);
     this.repaint();
   }
+
   @Override
   public void buildComposition(MusicModelObserver<Note> model) {
     this.pitches = model.pitchRangeAsList();
     this.notes = model.getComposition();
     this.maxBeats = model.maxBeat();
   }
+
+  /**
+   * To represent the notes visualized
+   */
+  private class NotePanel extends JPanel {
+    private java.util.List<Note> notes;
+    private java.util.List<String> pitches;
+    private int numPitches;
+    private int maxBeat;
+
+    /**
+     * Construct a note panel
+     *
+     * @param pitches pitches to use to vertically align the notes
+     * @param notes   the notes to display
+     * @param maxBeat the maximum beat of the notes
+     */
+    private NotePanel(java.util.List<String> pitches, java.util.List<Note> notes, int maxBeat) {
+      super();
+      this.notes = notes;
+      this.pitches = pitches;
+      this.numPitches = pitches.size();
+      this.maxBeat = maxBeat;
+      this.setPreferredSize(new Dimension(maxBeat * 15, (numPitches) * 15));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      this.generateGrid(g);
+    }
+
+    /**
+     * Generates the grid of notes, aligned to the pitches and labeled for each 16th beat.
+     *
+     * @param g the graphic onwhich everything is drawn on
+     */
+    private void generateGrid(Graphics g) {
+      for (int j = 0; j <= maxBeat; j++) {
+        if (j % 16 == 0)
+          g.drawString(Integer.toString(j), j * 15, 10);
+      }
+      for (int i = 1; i <= numPitches; i++) {
+        for (int j = 0; j < maxBeat; j++) {
+          if (j % 4 == 0) {
+            g.drawRect(j * 15, i * 15, 60, 15);
+          }
+        }
+      }
+      for (Note n : notes) {
+        int yPos = this.pitches.size() -
+                (this.pitches.indexOf(n.getPitch().toString() + n.getOctave()));
+        int xPos = n.getStartingBeat();
+        g.setColor(Color.BLACK);
+        g.fillRect(xPos * 15, yPos * 15, 15, 15);
+        xPos += 1;
+        g.setColor(Color.GREEN);
+        for (int i = xPos; i < xPos + n.getDuration() - 1; i++) {
+          g.fillRect(i * 15, yPos * 15, 15, 15);
+        }
+      }
+
+    }
+  }
+
+  /**
+   * To represent a JPanel for the list of pitches
+   */
+  private class PitchPanel extends JPanel {
+    private java.util.List<String> pitches;
+
+    /**
+     * To construct the Panel which displays the Pitch
+     *
+     * @param pitches the pitches to display on the panel
+     */
+    private PitchPanel(java.util.List<String> pitches) {
+      super();
+      this.pitches = pitches;
+      this.setPreferredSize(new Dimension(25, (pitches.size() + 1) * 15));
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      int yPos = (pitches.size() + 1) * 15;
+      for (String s : pitches) {
+        g.drawString(s, 0, yPos);
+        yPos -= 15;
+      }
+    }
+
+  }
+
 }
